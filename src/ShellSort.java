d5f2a5bb5942db617edbd518aff665a3b5cfbf85
jojamullamudi <jose.jamul2005@gmail.com>
2026-02-06 01:20:36 -0600

    Fixed creating b outside the inner for loop so it can be called outside it. Now fixed

--- src/ShellSort.java
+++ src/ShellSort.java
@@ -8,15 +8,36 @@ public class ShellSort implements SortingAlgorithm {
            for ( int a = gap; a < input.length; a++)
            {
                temp =  input[a];
-               for ( int b = a; b >= gap && temp < input[b - gap]; b = b -gap )
+               int b;
+               for ( b = a; b >= gap && temp < input[b - gap]; b = b -gap )
                {
-                   temp[b] = input[b - gap];
+                   input[b] = input[b - gap];
                }
-               input[a] = temp;
+               input[b] = temp;
            }
        }
 
-       return input
+       return input;
+    }
+/*
+
+    public static void main(String args[])
+    {
+        int[] input = {-5, 4, 2, 619, 11, 5, 620, -3};
+
+        ShellSort bs = new ShellSort();
+        bs.sorty(input);
+
+        System.out.print("[");
+        for (int i = 0; i < input.length; i++) {
+            System.out.print(input[i]);
+            if (i < input.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+
     }
 
+ */
 }

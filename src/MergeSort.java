9e5ff549b5ea516feb781409735609ac1f8f6837
jojamullamudi <jose.jamul2005@gmail.com>
2026-02-06 17:51:24 -0600

    Fixed the else if problem, then made sure the new array is printed.

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,9 +1,8 @@
-public class MeregSort implements SortingAlgorithm{
+public class MergeSort implements SortingAlgorithm{
 
     public int[] sorty(int[] input) {
 
-        mergesort(input);
-        return input;
+       return mergesort(input);
 
     }
 
@@ -22,17 +21,17 @@ public class MeregSort implements SortingAlgorithm{
                 m[a] = two[twoindex];
                 twoindex++;
             }
-            if ( twoindex == two.length)
+            else if ( twoindex == two.length)
             {
                 m[a] = one[oneindex];
                 oneindex++;
             }
-            if ( one[oneindex] <= two[twoindex])
+            else if ( one[oneindex] <= two[twoindex])
             {
                 m[a] = one[oneindex];
                 oneindex++;
             }
-            if ( one[oneindex] >= two[twoindex])
+            else if ( one[oneindex] >= two[twoindex])
             {
                 m[a] = two[twoindex];
                 twoindex++;
@@ -67,6 +66,27 @@ public class MeregSort implements SortingAlgorithm{
 
         return merge( one, two);
     }
+/*
+    public static void main(String args[])
+    {
+        int[] input = {-5, 4, 2, 619, 11, 5, 620, -3};
+
+        MergeSort bs = new MergeSort();
+        input = bs.sorty(input);
+
+        System.out.print("[");
+        for (int i = 0; i < input.length; i++) {
+            System.out.print(input[i]);
+            if (i < input.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+
+    }
+
+ */
+
 
 
 }

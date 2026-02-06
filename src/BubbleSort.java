e8f38579cc564086386389c56ade44007138879d
jojamullamudi <jose.jamul2005@gmail.com>
2026-02-06 00:21:19 -0600

    tested bubble sort, noticed was missing "-a" in b < input.length-a-1. Tested the sort with main method and it works!

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -5,7 +5,7 @@ public class BubbleSort implements SortingAlgorithm{
         int temp;
         for ( int a = 0; a < input.length -1; a++)
         {
-            for ( int b = 0; b < input.length-1; b++ )
+            for ( int b = 0; b < input.length-a-1; b++ )
             {
                 if ( input[b] > input[b+1])
                 {
@@ -19,8 +19,24 @@ public class BubbleSort implements SortingAlgorithm{
         return input;
 
     }
+        /*
+        public static void main(String args[])
+        {
+            int[] input = {-5, 4, 2, 619, 11, 5, 620, -3};
 
+            BubbleSort bs = new BubbleSort();
+            bs.sorty(input);
 
+            System.out.print("[");
+            for (int i = 0; i < input.length; i++) {
+                System.out.print(input[i]);
+                if (i < input.length - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println("]");
 
+        }
+       */
 
 }

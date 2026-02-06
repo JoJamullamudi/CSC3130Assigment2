611dd0e66873560ddb8b70464cd03de8d51737db
jojamullamudi <jose.jamul2005@gmail.com>
2026-02-06 00:36:22 -0600

    Tested Insertion Sort no problems!

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -19,6 +19,26 @@ public class InsertionSort implements SortingAlgorithm{
         return input;
 
     }
+/*
+    public static void main(String args[])
+    {
+        int[] input = {-5, 4, 2, 619, 11, 5, 620, -3};
+
+        InsertionSort bs = new InsertionSort();
+        bs.sorty(input);
+
+        System.out.print("[");
+        for (int i = 0; i < input.length; i++) {
+            System.out.print(input[i]);
+            if (i < input.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+
+    }
+
+ */
 
 
 
